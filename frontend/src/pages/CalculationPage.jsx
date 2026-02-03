41d895328edbb9cd183d149ab1701ed02545ca32
Yana Matsepura <yana.matsepura@zeb.de>
2026-02-03 14:51:28 +0100

    animation

--- frontend/src/pages/CalculationPage.jsx
+++ frontend/src/pages/CalculationPage.jsx
@@ -5,10 +5,33 @@ const CalculationPage = () => {
   const [amount, setAmount] = useState("");
   const [duration, setDuration] = useState("");
   const [investmentType, setInvestmentType] = useState("");
+  const [amountForTable, setAmountForTable] = useState("");
+  const [durationForTable, setDurationForTable] = useState("");
+  const [investmentTypeForTable, setInvestmentTypeForTable] = useState("");
+  const [isShifted, setIsShifted] = useState(false);
   const [results, setResults] = useState(null);
 
+  const numberFormatter = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  const lastRow =
+    results && results.rows && results.rows.length > 0
+      ? results.rows.at(-1)
+      : null;
+
+  const totalReturn = lastRow
+    ? lastRow.endKapital - results.rows[0].startKapital
+    : null;
+
   const handleCalculate = async () => {
+    if (!amount || !duration || duration <= 0) return;
+
     try {
+      setIsShifted(true);
       const response = await fetch("http://localhost:8080/api/calculate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -21,6 +44,14 @@ const CalculationPage = () => {
 
       const data = await response.json();
       setResults(data);
+
+      setAmountForTable(amount);
+      setDurationForTable(duration);
+      setInvestmentTypeForTable(investmentType);
+
+      setAmount("");
+      setDuration("");
+      setInvestmentType("");
     } catch (error) {
       console.error(error);
     }
@@ -28,44 +59,123 @@ const CalculationPage = () => {
 
   return (
     <main className="calculation-page">
-      <div className="calculation-page-content">
-        {results === null ? <div className="calculation-page-text">
-          <h2 className="calculation-page-title">Investment Calculator</h2>
-          <p className="calculation-page-description">Enter your investment details below to see how your money could grow over time. Adjust the amount and duration to explore different scenarios and better understand potential outcomes.</p>
-        </div> : <><div className="table-container">
-          <table className="result-table">
-            <thead>
-              <tr>
-                <th className="table-header">Year</th>
-                <th className="table-header">Start Capital</th>
-                <th className="table-header">Return (%)</th>
-                <th className="table-header">End Capital</th>
-              </tr>
-            </thead>
-            <tbody>
-              {results.rows.map((row, index) => (<tr key={index}>
-                <td className="result-table-row">{row.jahr}</td>
-                <td className="result-table-row">{row.startKapital.toFixed(2)}</td>
-                <td className="result-table-row">{row.rendite.toFixed(2)}</td>
-                <td className="result-table-row">{row.endKapital.toFixed(2)}</td>
-              </tr>))}
-            </tbody>
-          </table>
-          <p className="total-return"><strong>Total Return:</strong> {(results.rows[duration - 1].endKapital - results.rows[0].startKapital).toFixed(2)}</p>
-          <p className="end-capital"><strong>End Capital:</strong> {results.rows[duration - 1].endKapital.toFixed(2)}</p>
-        </div> </>}
-        <div className="calculation-form"> <label htmlFor="investment-amount">Investment Amount:</label>
-          <input type="text" id="investment-amount" placeholder="Enter investment amount" label="Investment Amount" value={amount} onChange={(e) => setAmount(e.target.value)} /><br />
-          <label htmlFor="investment-duration">Investment Duration in years:</label>
-          <input type="text" placeholder="Enter investment duration in years" label="Investment Duration" value={duration} onChange={(e) => setDuration(e.target.value)} /><br />
-          <label htmlFor="investment-type">Investment type:</label> <input list="investment-type-list" id="investment-type" name="investment-type" placeholder="Choose your investment type" value={investmentType} onChange={(e) => setInvestmentType(e.target.value)} />
-          <datalist id="investment-type-list"> <option value="Shares" /> <option value="Bonds" /> <option value="Property" /> <option value="Securities" />
-          </datalist><br /><br />
+      <div
+        className={`calculation-page-content ${
+          results ? "w3-animate-right" : ""
+        }`}>
+        {!results && (
+          <div className="calculation-page-text">
+            <h2 className="calculation-page-title">Investment Calculator</h2>
+            <p className="calculation-page-description">
+              Enter your investment details below to see how your money could
+              grow over time. Adjust the amount and duration to explore
+              different scenarios and better understand potential outcomes.
+            </p>
+          </div>
+        )}
+
+        {results ? (
+          <>
+            <div className="calculation-form"></div>
+            <div className="table-container">
+              <div className="investment-info">
+                <p>
+                  <strong>Amount:</strong>{" "}
+                  {numberFormatter.format(Number(amountForTable))} |{" "}
+                  <strong>Duration:</strong> {Number(durationForTable)} years |{" "}
+                  <strong>Investment type:</strong> {investmentTypeForTable}
+                </p>
+              </div>
+              <table className="result-table">
+                <thead>
+                  <tr>
+                    <th className="table-header">Year</th>
+                    <th className="table-header">Start Capital</th>
+                    <th className="table-header">Return (%)</th>
+                    <th className="table-header">End Capital</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {results.rows.map((row, index) => (
+                    <tr key={index}>
+                      <td className="result-table-row">{row.jahr}</td>
+                      <td className="result-table-row">
+                        {numberFormatter.format(row.startKapital)}
+                      </td>
+                      <td className="result-table-row">
+                        {row.rendite.toFixed(2)}
+                      </td>
+                      <td className="result-table-row">
+                        {numberFormatter.format(row.endKapital)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {lastRow && (
+                <>
+                  <p className="total-return">
+                    <strong>Total Return:</strong>{" "}
+                    {numberFormatter.format(totalReturn)}
+                  </p>
+                  <p className="end-capital">
+                    <strong>End Capital:</strong>{" "}
+                    {numberFormatter.format(lastRow.endKapital)}
+                  </p>
+                </>
+              )}
+            </div>
+          </>
+        ) : (
+          <div className="calculation-form"></div>
+        )}
+        <div
+          className={`calculation-form overlay-form ${isShifted ? "shifted" : ""}`}>
+          <label htmlFor="investment-amount">Investment Amount:</label>
+          <input
+            type="number"
+            id="investment-amount"
+            placeholder="Enter investment amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <br />
+          <label htmlFor="investment-duration">
+            Investment Duration in years:
+          </label>
+          <input
+            type="number"
+            placeholder="Enter investment duration in years"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          />
+          <br />
+          <label htmlFor="investment-type">Investment type:</label>
+          <input
+            list="investment-type-list"
+            id="investment-type"
+            name="investment-type"
+            placeholder="Choose your investment type"
+            value={investmentType}
+            onChange={(e) => setInvestmentType(e.target.value)}
+          />
+          <datalist id="investment-type-list">
+            <option value="Shares" />
+            <option value="Bonds" />
+            <option value="Property" />
+            <option value="Securities" />
+          </datalist>
+          <br />
+          <br />
           <div className="calculation-button-container">
-            <button type="button" onClick={handleCalculate}>Calculate</button> </div>
+            <button type="button" onClick={handleCalculate}>
+              {results ? "Calculate again" : "Calculate"}
+            </button>
+          </div>
         </div>
       </div>
-    </main>)
+    </main>
+  );
 };
 
 export default CalculationPage;
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,14 +1,20 @@
-import '../styles/MainPage.css'
+import "../styles/MainPage.css";
 
 const MainPage = () => {
   return (
     <main className="main-page">
       <h2 className="main-page-title">Calculate Your Future</h2>
-      <p className="main-page-description">How will your investment grow over time?
-        Enter your data and instantly get a clear overview of potential returns. Our investment calculator helps you compare different scenarios and make confident decisions aligned with your financial goals.</p>
-        <button className='main-page-button'><a href="/calculation">Start calculation</a></button>
+      <p className="main-page-description">
+        How will your investment grow over time? Enter your data and instantly
+        get a clear overview of potential returns. Our investment calculator
+        helps you compare different scenarios and make confident decisions
+        aligned with your financial goals.
+      </p>
+      <a href="/calculation">
+        <button className="main-page-button">Start calculation</button>
+      </a>
     </main>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
